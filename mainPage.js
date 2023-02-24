128ee06ffb73ac613c2070bf0af76d2d83745ac2
Modiperin <modiperin@gmail.com>
2023-02-24 17:40:54 +0530

    modalButNotWorking

--- mainPage.js
+++ mainPage.js
@@ -10,3 +10,35 @@ function callStock(stockname){
     window.sessionStorage.setItem('stockname',stockname)
     window.location.href='stockStructure.html'
 }
+
+// let elementsArray = document.querySelectorAll("planButton");
+
+// elementsArray.forEach(function(elem) {
+//     elem.addEventListener("click", function() {
+//         console.log('perin')
+//     });
+// });
+
+// document.querySelectorAll(".planButton").forEach(elem => elem.addEventListener("click", ()=>{
+//     console.log('perin')
+//     if(loggedUser!=null)
+// {
+//     document.getElementById('myModal').style.display="none"
+// }
+// }))
+// document.getElementById('go').addEventListener('click',()=>{
+//     // if(loggedUser==null)
+//     // {
+//         $('#myModal').modal('hide');
+//     // }
+//     $("#myModal").on('hide.bs.modal',()=> {
+//         alert('The modal is about to be hidden.');
+//     });
+// })
+// $('#go').click(function() {
+//     // var test1 = $('#test').val();
+//     if(loggedUser==null)
+//     {
+//         $('#\\#myModal').modal('hide');
+//     }
+//   });
